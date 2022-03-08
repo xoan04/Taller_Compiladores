89c6be1c88506180818348ccc1a76eaf2cbb24fd
Juan <juangomezjs@unimagdalena.edu.co>
2022-03-08 10:40:47 -0500

    Version 1.1

--- Libreria.py
+++ Libreria.py
@@ -1,5 +1,49 @@
-from xml.etree.ElementTree import parse,Element
+import xml.etree.ElementTree as ET
+from Lib import *
+from tkinter import *
+from tkinter import ttk
 xml = 'biblioteca.xml'
-xmlParse= parse(xml)
+xmlParse=ET.parse(xml)
 raiz = xmlParse.getroot()
-print(raiz)
+ET.dump(xmlParse)
+
+class VentanaPrincipal(ttk.Frame):
+    def __init__(self,main_window):
+        super().__init__(main_window)
+        main_window.title("Biblioteca")
+        main_window.geometry("500x500")
+        self.treeview=ttk.Treeview(self)
+    for child in raiz:
+        print(child.tag, child.text)
+        
+for Libro in raiz.findall('Libro'):
+ genero = Libro.find('Genero').text
+ titulo= Libro.get('Titulo')
+ print("\n")
+ print(titulo,"\n",genero)
+        
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+root=Tk()
+ventana=VentanaPrincipal(root)
+ventana.mainloop()
--- Tkin.py
+++ Tkin.py
@@ -4,12 +4,10 @@ import Libreria as Lib
 from Lib import *
 from tkinter import *
 from tkinter import ttk
-
-root= Tk()
-root.resizable(1,1)
-tree=ttk.Treeview(root,columns=("#1","#2"))
-tree.pack()
-tree.heading("#0",text="ID")
-tree.insert("",END,text=Lib.raiz)
-root.mainloop()
-#ola
+main_window = Tk()
+main_window.title("Vista de árbol en Tkinter")
+treeview = ttk.Treeview()
+item = treeview.insert("", END, text="Elemento 1")
+treeview.insert(item, END, text="Subelemento 1")
+treeview.pack()
+main_window.mainloop()
