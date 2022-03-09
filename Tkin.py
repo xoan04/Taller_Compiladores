29e2a53231592d26b1e5dfc176ec8ede2ca8087f
Juan <juangomezjs@unimagdalena.edu.co>
2022-03-08 23:24:58 -0500

    Entrega Final

--- Tkin.py
+++ Tkin.py
@@ -1,13 +1,52 @@
-import sys
-sys.path.append("C:/Users/Juan/Documents/VisualSpy/Biblioteca/Libreria.py")
-import Libreria as Lib
-from Lib import *
 from tkinter import *
-from tkinter import ttk
+from tkinter import ttk, messagebox
+import xml.etree.ElementTree as ET
+
+
+
 main_window = Tk()
 main_window.title("Vista de árbol en Tkinter")
+main_window.geometry("400x400")
+
+tree = ET.parse("biblioteca.xml")
+root = tree.getroot()
 treeview = ttk.Treeview()
-item = treeview.insert("", END, text="Elemento 1")
-treeview.insert(item, END, text="Subelemento 1")
-treeview.pack()
+token_root_iid = 210
+token_libro_iid = 1
+libros = []
+
+treeview.insert("", END, text="biblioteca", iid=token_root_iid)
+"primer nivel"
+for item in root.findall("./"): 
+    libros.append(item.attrib)
+
+for item in libros:
+    treeview.insert(token_root_iid, END, text="libros", iid=token_libro_iid)
+    token_libro_iid += 1
+
+"segundo nivel"
+token_libros_atrib_iid = 1
+dic_genero = {}
+dic_publicacion = {}
+dic_autor = {}
+for item in root.findall("./Libro/"):
+    match item.tag:
+        case "Genero":
+            dic_genero[item.tag] = item.text
+        case "Publicacion":
+            dic_publicacion[item.tag] = item.text
+        case "Autor":
+            dic_autor[item.tag] = item.text
+        case _:
+            print("error?")
+
+cantidad_libros = len(libros)
+for item in range(cantidad_libros):
+    treeview.insert((item + 1), END, text="genero", iid=None)
+    treeview.insert((item + 1), END, text="publicacion", iid=None)
+    treeview.insert((item + 1), END, text="autor", iid=None)
+
+
+
+treeview.pack(fill = BOTH, expand = True)
 main_window.mainloop()
---
+++ Tkinpoo.py
@@ -0,0 +1,41 @@
+from tkinter import *
+from tkinter import ttk, messagebox
+import xml.etree.ElementTree as ET
+
+class AppXmlInPy(ttk.Frame):
+
+    def __init__(self, main_window):
+        
+        super().__init__(main_window)
+        main_window.title("XML viewer")
+        main_window.geometry("500x500")
+        self.treeview = ttk.Treeview(self)
+        "invocamos la raiz con el ET parse y el getroot()"
+        self.tree = ET.parse('biblioteca.xml')
+        self.raiz = self.tree.getroot()
+        "aqui insertamos el primer tag y lo guardamos como referencia para insertar lo demas"
+        self.nodo_principal = self.treeview.insert("", END, None, text=self.raiz.tag)
+        "esa raiz se la pasamos a la funcion que dibujara el arbol"
+        self.insert_treeview(self.raiz, parent=self.nodo_principal)
+        "opciones para visualizar mejor el treeview"
+        self.treeview.pack(fill = BOTH, expand = True)
+        self.pack(fill = BOTH, expand = True)
+    
+    def insert_treeview(self, raiz, parent=""):
+        "recorremos la raiz"
+        for child in raiz:
+            node_name = child.tag
+            item = self.treeview.insert(parent, END, None, text=node_name)
+            if len(child) > 0:
+                "si tiene hijos volvemos a invocar"
+                self.insert_treeview(child, parent=item)
+            else:
+                """si no tiene hijos solo insertamos pero con el text
+                se supone que el ultimo nivel es donde se guardan los valores en el
+                campo de texto"""
+                if(child.text != None):
+                    self.treeview.insert(item, END, None, text=child.text)
+
+root = Tk()
+app = AppXmlInPy(root)
+app.mainloop()
